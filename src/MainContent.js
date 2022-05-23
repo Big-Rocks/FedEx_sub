0830d5b5b0a8646160d4f0e48de00b7da01ff44e
Big-Rocks <bigrocks89@outlook.com>
2022-05-23 06:38:15 +0300

    may 23rd

---
+++ src/MainContent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function MainContent(){
+    return (
+        <>
+            <h3>React course</h3>
+            <ol>
+                <li>What is the pros of React.js</li>
+                <li>Cons of React.js</li>
+                <li>Will the web world become react.</li>
+            </ol>
+        </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,39 +1,12 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
+import Header from "./Header"
+import Footer from "./Footer"
+import MainContent from "./MainContent"
+import "./index.css"
 
 const container1 = document.querySelector('#root');
 const root = createRoot(container1);
-
-function Header(){
-  return (
-    <header>
-      <nav>
-        <img src="logo512.png" width="30px" />
-      </nav>
-    </header>
-  );
-}
-
-function Footer(){
-  return (
-    <footer>
-      <small>© 2022 Bigrocks development. All rights reserved.</small>
-    </footer>
-  );
-}
-
-function MainContent(){
-  return (
-    <>
-      <h1>Some jokes</h1>
-      <ol>
-        <li>Now I am getting affinity about react.</li>
-        <li>It's a little bit interesting</li>
-        <li>Quite boring but someday I would master it.</li>
-      </ol>
-    </>
-  );
-}
 
 function Page() {
   return (
